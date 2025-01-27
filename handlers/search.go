0fa465550c8018531dc43e77b6650e3329547248
Vess Nedkov <2430850+vnedkov@users.noreply.github.com>
2025-01-27 10:50:47 -0500

    UFF-7 Added parameter validation, filtering by hall ID

--- handlers/search.go
+++ handlers/search.go
@@ -2,9 +2,13 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"foodfinder-api/elasticsearch"
 	"net/http"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
@@ -12,24 +16,49 @@ import (
 
 const (
 	defaultPageSize = 10
+	minSearchLength = 3
+	maxSearchLength = 100
 )
 
 // SearchHandler is an HTTP handler that performs a search query against the Elasticsearch index
 func SearchHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the search query from the request
-	q := r.URL.Query().Get("q")
-	from := r.URL.Query().Get("from")
-	to := r.URL.Query().Get("to")
-	pageStr := r.URL.Query().Get("page")
-	sizeStr := r.URL.Query().Get("size")
-	// hallStr := r.URL.Query().Get("hall")
+	var err error
+	// Parse the URL query parameters
+	queryParams := r.URL.Query()
+	q := queryParams.Get("q")
+	fromStr := queryParams.Get("from")
+	toStr := queryParams.Get("to")
+	pageStr := queryParams.Get("page")
+	sizeStr := queryParams.Get("size")
+	// Get all selected halls. If no hall is selected, get all halls
+	hallStr := queryParams["hall"]
 
 	// Validate the query parameters
-	// TODO: Implement validation for the query string
+	if err := validateSearchString(q); handleError(w, r, err, http.StatusBadRequest) {
+		return
+	}
+	var fromDate time.Time
+	// Validate the date parameters
+	if fromDate, err = validateDateString(fromStr, time.Now()); handleError(w, r, err, http.StatusBadRequest) {
+		return
+	}
+
+	var toDate time.Time
+	if toDate, err = validateDateString(toStr, time.Now().AddDate(0, 0, 14)); handleError(w, r, err, http.StatusBadRequest) {
+		return
+	}
+
+	// Validate the hall parameters
+	var halls []int
+	if halls, err = validateHalls(hallStr); handleError(w, r, err, http.StatusBadRequest) {
+		return
+	}
+
+	// Parse the page and size parameters
 	page := 0
 	if pageStr != "" {
-		if p, err := strconv.Atoi(pageStr); err != nil {
-			_ = handleError(w, r, err, http.StatusBadRequest, "Invalid page number")
+		if p, err := strconv.Atoi(pageStr); handleError(w, r, err, http.StatusBadRequest, "Invalid page number ", pageStr) {
+			return
 		} else {
 			page = p
 		}
@@ -37,22 +66,11 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 
 	pageSize := defaultPageSize
 	if sizeStr != "" {
-		if s, err := strconv.Atoi(sizeStr); err != nil {
-			_ = handleError(w, r, err, http.StatusBadRequest, "Invalid page size")
+		if s, err := strconv.Atoi(sizeStr); handleError(w, r, err, http.StatusBadRequest, "Invalid page size ", sizeStr) {
+			return
 		} else {
 			pageSize = s
 		}
-	}
-
-	if from == "" {
-		from = time.Now().Format("2006-01-02")
-	} else if _, err := time.Parse("2006-01-02", from); err != nil {
-		_ = handleError(w, r, err, http.StatusBadRequest, "Invalid from date format")
-	}
-	if to == "" {
-		to = time.Now().AddDate(0, 0, 15).Format("2006-01-02")
-	} else if _, err := time.Parse("2006-01-02", to); err != nil {
-		_ = handleError(w, r, err, http.StatusBadRequest, "Invalid to date format")
 	}
 
 	// Create a new Elasticsearch client
@@ -61,19 +79,20 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	query := getQueryFilters(&from, &to, &q)
+	// Elasticsearch query needs *string for date comparisons
+	from := fromDate.Format("2006-01-02")
+	to := toDate.Format("2006-01-02")
 
 	// Perform the search query
 	res, err := es.Search().
 		Index(elasticsearch.Index).
-		Query(query).
+		Query(getQuery(&from, &to, &q, halls)).
 		From(page * pageSize).
 		Size(pageSize).
 		Pretty(true).
 		Do(r.Context())
 
-	if err != nil {
-		http.Error(w, "Error performing search query", http.StatusInternalServerError)
+	if handleError(w, r, err, http.StatusInternalServerError, "Error performing search query") {
 		return
 	}
 
@@ -82,7 +101,7 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	for _, hit := range res.Hits.Hits {
 		var food map[string]interface{}
 		if err := json.Unmarshal(hit.Source_, &food); err != nil {
-			http.Error(w, "Error decoding search response", http.StatusInternalServerError)
+			_ = handleError(w, r, err, http.StatusInternalServerError, "Error decoding search response")
 			return
 		}
 		searchResponse = append(searchResponse, food)
@@ -93,7 +112,8 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(searchResponse)
 }
 
-func getQueryFilters(from, to, q *string) *types.Query {
+// getQuery creates a new Elasticsearch query based on the given parameters
+func getQuery(from, to, q *string, halls []int) *types.Query {
 	// Create a new query filter
 	query := types.Query{
 		Bool: &types.BoolQuery{
@@ -118,5 +138,75 @@ func getQueryFilters(from, to, q *string) *types.Query {
 			},
 		},
 	}
+	// Add the halls filter if provided
+	if len(halls) > 0 {
+		query.Bool.Filter = append(query.Bool.Filter, types.Query{
+			Terms: &types.TermsQuery{
+				TermsQuery: map[string]types.TermsQueryField{
+					"hall_id": halls,
+				},
+			},
+		})
+	}
 	return &query
+}
+
+// validateDateString validates the given date string based on specific criteria
+func validateDateString(date string, defaultIfEmpty time.Time) (time.Time, error) {
+	// If the date string is empty, return the default date
+	if date == "" {
+		return defaultIfEmpty, nil
+	}
+	if t, err := time.Parse("2006-01-02", date); err != nil {
+		return time.Time{}, err
+	} else {
+		return t, nil
+	}
+}
+
+// validateSearchString validates the given search string based on specific criteria
+func validateSearchString(query string) error {
+	// Trim whitespace
+	query = strings.TrimSpace(query)
+
+	// Check for empty query
+	if query == "" {
+		return errors.New("search query cannot be empty")
+	}
+
+	// Length constraints
+	if len(query) < minSearchLength {
+		return fmt.Errorf("search query must be at least %d characters long", minSearchLength)
+	}
+	if len(query) > maxSearchLength {
+		return fmt.Errorf("search query cannot exceed %d characters", maxSearchLength)
+	}
+
+	// Allowed characters: alphanumeric and spaces
+	validPattern := `^[a-zA-Z0-9\s]+$`
+	matched, err := regexp.MatchString(validPattern, query)
+	if err != nil {
+		return fmt.Errorf("error validating query: %v", err)
+	}
+	if !matched {
+		return errors.New("search query can only contain alphanumeric characters and spaces")
+	}
+
+	// All validations passed
+	return nil
+}
+
+func validateHalls(halls []string) ([]int, error) {
+	var validHalls []int
+	for _, hall := range halls {
+		if hall == "" {
+			continue
+		}
+		hallID, err := strconv.Atoi(hall)
+		if err != nil {
+			return nil, fmt.Errorf("invalid hall ID: %s", hall)
+		}
+		validHalls = append(validHalls, hallID)
+	}
+	return validHalls, nil
 }
