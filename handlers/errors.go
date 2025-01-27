f7a0087d8c021c63247bab0bd54401f8596d77b1
Vess Nedkov <2430850+vnedkov@users.noreply.github.com>
2025-01-27 07:23:15 -0500

    UFF-5 Improved search query; Added logging and error response

--- handlers/errors.go
+++ handlers/errors.go
@@ -1,19 +1,28 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 )
 
+// errorResponse is a struct that represents an error response
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
 // handleError is a helper function that writes an error response to the client
 func handleError(w http.ResponseWriter, r *http.Request, err error, responseStatus int, msg ...string) bool {
-	var errMsg string
+	errMsg := strings.Join(msg, "")
 	if err == nil {
 		return false
 	}
 
 	log.Err(err).Msg(errMsg)
-	http.Error(w, errMsg, responseStatus)
+
+	resp, _ := json.Marshal(errorResponse{Error: errMsg})
+	http.Error(w, string(resp), responseStatus)
 	return true
 }
